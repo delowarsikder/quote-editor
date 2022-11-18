2b0881b5ecdbbf20fb36b33d72eaa85a067cb5e7
delowarsikder <delowarsikder099@gmail.com>
2022-11-18 18:41:49 +0600

    add line item and product price

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,17 @@ module ApplicationHelper
     turbo_stream.prepend "flash", partial: "layouts/flash"
   end
 
+  def form_error_notification(object)
+    if object.errors.any?
+      tag.div class: "error-message" do
+        object.errors.full_messages.to_sentence.capitalize
+      end
+    end
+  end
+
+
+  def nested_dom_id(*args)
+    args.map { |arg| arg.respond_to?(:to_key) ? dom_id(arg) : arg }.join("_")
+  end
+
 end
---
+++ test/controllers/line_items_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class LineItemsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get line_items_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get line_items_create_url
+    assert_response :success
+  end
+end
